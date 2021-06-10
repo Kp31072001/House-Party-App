ab7beb373114ac0c7eaf9aa88264729428fe70fb
Kp31072001 <kankanparamanik001@gmail.com>
2021-06-10 05:32:30 +0530

    Front end Upgraded

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom'
 
+import HomePage from './HomePage';
+
+
 export default class App extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+
+        }
     }
 
     render() {
-        return <h1>{this.props.name}</h1>
+        return (
+            <div>
+                <HomePage />
+
+            </div>
+        )
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App name="kankan"/>, appDiv);
+render(<App />, appDiv);
