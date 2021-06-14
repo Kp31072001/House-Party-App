b3497d4260c36f637586c35c6e0d02a6f9de1f33
Kp31072001 <kankanparamanik001@gmail.com>
2021-06-15 05:02:03 +0530

    final commit

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,25 +1,44 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom'
 
-import HomePage from './HomePage';
 
+// import React, { Component } from "react";
+// import { render } from "react-dom";
+// import HomePage from "./HomePage";
+
+// export default class App extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div className='center'>
+//         <HomePage />
+//       </div>
+//     );
+//   }
+// }
+
+// const appDiv = document.getElementById("app");
+// render(<App />, appDiv);
+
+
+
+import React, { Component } from "react";
+import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+  }
 
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <HomePage />
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="center">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 const appDiv = document.getElementById("app");
---
+++ music_controller/spotify/credentials.py
@@ -0,0 +1,3 @@
+CLIENT_ID = "d61ce99e16bc4ee3b93a205b12f3add1"
+CLIENT_SECRET = "e10edd4495c6427aabd6e7239830d9ee"
+REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
